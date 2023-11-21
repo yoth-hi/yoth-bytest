3662403932f32f8e4d482c66f814c7166dec88f6
yoth-hi <levifelix063@gmail.com>
2023-11-20 21:29:32 -0300

    0.0.3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,35 @@
 
 
 const nextConfig = {
-  
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {  
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {  
+            key: "Expires",
+            value: "max-age=31536000",
+          },
+          {  
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+          {  
+            key: "Pragma",
+            value: "public",
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
