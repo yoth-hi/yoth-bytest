0458c66d1e649313393cfbcbf24b3abb7de277e9
yoth-hi <levifelix063@gmail.com>
2023-11-20 21:38:43 -0300

    0.0.4-belittling-the-performance

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/',
+        source: '/:path*',
         headers: [
           {
             key: "Strict-Transport-Security",
