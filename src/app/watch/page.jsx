0d832a6d892d6f72c5e32578515ecbf7a8016da7
yoth-hi <levifelix063@gmail.com>
2023-11-25 11:06:39 -0300

    0.0.5

--- src/app/watch/page.jsx
+++ src/app/watch/page.jsx
@@ -2,20 +2,12 @@ import Page from "./_page"
 import Player from "../../components/player";
 import Fetch from "../../service/ApiRest";
 import { redirect } from 'next/navigation'
-
-interface RootProps {
-  searchParams: {
-    tw?: string;
-    v?: string;
-  };
-}
-
-export default async function Root(props: RootProps){
+export default async function Root(props){
   const { searchParams: { tw, v } } = props;
-  const platform: string | null = tw ? "twitch" : (v ? "youtube" : null );
-  const id: string | null = tw || v;
+  const platform = tw ? "twitch" : (v ? "youtube" : null );
+  const id = tw || v;
   if(!id){return redirect("/")}
-  const data: any = await Fetch({
+  const data = await Fetch({
       type: "browse",
       context: {
         type: "player_page",
@@ -30,4 +22,4 @@ export default async function Root(props: RootProps){
   }
   return(<Page {...newProps} data={data} key={276} />)
 }
-export const revalidate: number = 60;
+export const revalidate = 60;
