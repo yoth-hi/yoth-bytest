50dce8a3f6f6673e9e561851b06436732b45567c
yoth-hi <levifelix063@gmail.com>
2023-11-29 23:12:03 -0300

    0.0.6

--- public/s/player.ts
+++ public/s/player.ts
@@ -1,9 +1,14 @@
+try {
 type PropsSetPlayerDataType = {
   mode: 'fullscreen' | 'theater' | 'watch' | 'miniplayer',
   id: string
 }
-const t: object = (window.yoth=window.yoth||{});
+const t: object = (globalThis.yoth=globalThis.yoth||{});
 const setPlayerDataType = function({}:PropsSetPlayerDataType) {
   
 }
 t.setPlayerDataType=setPlayerDataType;
+  
+} catch (error) {
+  
+}
