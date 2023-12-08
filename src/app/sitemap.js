5beb5b6075338f230e9ea293a74abb7194c790a0
yoth-hi <levifelix063@gmail.com>
2023-12-08 20:12:08 -0300

    0.0.7

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,13 +1,15 @@
-import { MetadataRoute } from 'next';
-
+import { MetadataRoute } from "next";
 
 export default async function sitemap() {
   try {
-    const origin: string = "https://yoth-hi.vercel.app";
-    const a: Response = await fetch(origin + "/yothpi/browse?key=27626272672626262636363636626262627393829", {
-      method: "GET",
-      next: { revalidate: 10 }
-    });
+    const origin = "https://yoth-hi.vercel.app";
+    const a = await fetch(
+      origin + "/yothpi/browse?key=27626272672626262636363636626262627393829",
+      {
+        method: "GET",
+        next: { revalidate: 10 },
+      }
+    );
     const b = (await a.json()) || {};
     const m = b?.list?.map((a) => {
       a.url = origin + a.url;
@@ -28,25 +30,23 @@ export default async function sitemap() {
         lastModified: new Date().toISOString(),
       },
     ];
-
-  } catch (error) {
-
-  }
-  return [{
-    url: origin + "/",
-    lastModified: new Date().toISOString(),
-  },
-  {
-    url: origin + "/gaming",
-    lastModified: new Date().toISOString(),
-  },
-  {
-    url: origin + "/following",
-    lastModified: new Date().toISOString(),
-  },
-  {
-    url: origin + "/watch?tw=wuant",
-    lastModified: new Date().toISOString(),
-  },
-  ]
+  } catch (error) {}
+  return [
+    {
+      url: origin + "/",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: origin + "/gaming",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: origin + "/following",
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: origin + "/watch?tw=wuant",
+      lastModified: new Date().toISOString(),
+    },
+  ];
 }
