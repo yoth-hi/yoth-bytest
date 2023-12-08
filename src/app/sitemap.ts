d309a0bcf4e3db05e81d07d1261e7702599c1ee2
yoth-hi <levifelix063@gmail.com>
2023-12-08 20:04:40 -0300

    0.0.7

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,52 +5,52 @@ interface SitemapItem {
   lastModified?: string;
 }
 
-export default async function sitemap(): Promise<SitemapItem[]> {
+export default async function sitemap(): MetadataRoute.Sitemap {
   try {
-  const origin: string = "https://yoth-hi.vercel.app";
-  const a: Response = await fetch(origin+"/yothpi/browse?key=27626272672626262636363636626262627393829", {
-    method: "GET",
-    next: { revalidate: 10 }
-  });
-  const b: any = await a.json() || {};
-  const m: SitemapItem[] = b?.list?.map((a: any): SitemapItem => {
-    a.url = origin + a.url;
-    return a;
-  });
-  return [
-    ...m,
-    {
-      url: origin + "/",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: origin + "/gaming",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: origin + "/following",
-      lastModified: new Date().toISOString(),
-    },
-  ];
-    
+    const origin: string = "https://yoth-hi.vercel.app";
+    const a: Response = await fetch(origin + "/yothpi/browse?key=27626272672626262636363636626262627393829", {
+      method: "GET",
+      next: { revalidate: 10 }
+    });
+    const b: any = await a.json() || {};
+    const m: SitemapItem[] = b?.list?.map((a: any): SitemapItem => {
+      a.url = origin + a.url;
+      return a;
+    });
+    return [
+      ...m,
+      {
+        url: origin + "/",
+        lastModified: new Date().toISOString(),
+      },
+      {
+        url: origin + "/gaming",
+        lastModified: new Date().toISOString(),
+      },
+      {
+        url: origin + "/following",
+        lastModified: new Date().toISOString(),
+      },
+    ];
+
   } catch (error) {
-    
+
   }
-    return[    {
-      url: origin + "/",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: origin + "/gaming",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: origin + "/following",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: origin + "/watch?tw=wuant",
-      lastModified: new Date().toISOString(),
-    },
-    ]
+  return [{
+    url: origin + "/",
+    lastModified: new Date().toISOString(),
+  },
+  {
+    url: origin + "/gaming",
+    lastModified: new Date().toISOString(),
+  },
+  {
+    url: origin + "/following",
+    lastModified: new Date().toISOString(),
+  },
+  {
+    url: origin + "/watch?tw=wuant",
+    lastModified: new Date().toISOString(),
+  },
+  ]
 }
