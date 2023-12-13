b1cc1d490abcc20f9af53729f66868d75917fce1
yoth-hi <levifelix063@gmail.com>
2023-12-13 20:16:22 -0300

    0.0.8

--- src/app/GoogleAnalytics.tsx
+++ src/app/GoogleAnalytics.tsx
@@ -3,9 +3,9 @@ export default function () {
     <>
       <script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-5RGFY1DM7H"
+        src="https://www.googletagmanager.com/gtag/js?id=G-WN77FWC13V"
       ></script>
-      <script>{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)};gtag('js',new Date());gtag('config','G-5RGFY1DM7H');`}</script>
+      <script>{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)};gtag('js',new Date());gtag('config','G-WN77FWC13V');`}</script>
     </>
   );
 }
